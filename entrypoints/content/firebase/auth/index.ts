60abad3bb3f964f9366ba20b7bd4c0818d7eeca3
Dirag Biswas <hey@diragb.dev>
2024-07-30 04:54:38 +0530

    feat: added logging to database and auth functions of firebase

--- entrypoints/content/firebase/auth/index.ts
+++ entrypoints/content/firebase/auth/index.ts
@@ -10,11 +10,12 @@ import { auth } from '..'
 import { FirebaseError } from 'firebase/app'
 import returnable from '../../utils/returnable'
 import { getRDBUserSnapshot } from '../realtime-database/user'
+import logError from '../../utils/logError'
 
 // Typescript:
 import { type UserCredential } from 'firebase/auth'
 import type { Returnable } from '../../types'
-import { Toast } from '../../components/ui/use-toast'
+import type { Toast } from '../../components/ui/use-toast'
 
 export enum AUTH_MODE {
   LOGIN,
@@ -68,50 +69,92 @@ export const authenticateWithEmailAndPassword = async (
       throw new Error('Invalid authentication mode!')
     }
   } catch (error) {
-    console.error(error)
-    if ((error as FirebaseError).code) {
-      const code = (error as FirebaseError).code as typeof AuthErrorCodes[keyof typeof AuthErrorCodes]
-      if (code === AuthErrorCodes.USER_DELETED) {
-        // NOTE: User does not exist. Move to createUser flow.
-        return returnable.fail({ isSuccessful: false, shouldRetry: false })
-      } else if (
-        code === AuthErrorCodes.USER_CANCELLED ||
-        code === AuthErrorCodes.USER_DISABLED
-      ) {
-        toast({
-          variant: 'destructive',
-          title: 'Your account was suspended!',
-          description: 'Please contact us to learn more.'
-        })
+    if (mode === AUTH_MODE.LOGIN) {
+      if ((error as FirebaseError).code) {
+        const code = (error as FirebaseError).code as typeof AuthErrorCodes[keyof typeof AuthErrorCodes]
+        if (code === AuthErrorCodes.USER_DELETED) {
+          // NOTE: User does not exist. Move to createUser flow.
+          return returnable.fail({ isSuccessful: false, shouldRetry: false })
+        } else if (
+          code === AuthErrorCodes.USER_CANCELLED ||
+          code === AuthErrorCodes.USER_DISABLED
+        ) {
+          toast({
+            variant: 'destructive',
+            title: 'Your account was suspended!',
+            description: 'Please contact us to learn more.'
+          })
 
-        return returnable.fail({ isSuccessful: false, shouldRetry: true })
-      } else if (
-        code === AuthErrorCodes.INVALID_EMAIL ||
-        code === AuthErrorCodes.INVALID_PASSWORD
-      ) {
-        toast({
-          variant: 'destructive',
-          title: 'Invalid email address or password!',
-          description: 'Please enter the correct email and password.',
-        })
+          return returnable.fail({ isSuccessful: false, shouldRetry: true })
+        } else if (
+          code === AuthErrorCodes.INVALID_EMAIL ||
+          code === AuthErrorCodes.INVALID_PASSWORD
+        ) {
+          toast({
+            variant: 'destructive',
+            title: 'Invalid email address or password!',
+            description: 'Please enter the correct email and password.',
+          })
 
-        return returnable.fail({ isSuccessful: false, shouldRetry: true })
-      } else if (code === AuthErrorCodes.NEED_CONFIRMATION) {
-        toast({
-          variant: 'destructive',
-          title: 'Please try signing in with Google!',
-          description: 'You signed up with Google, please log in with it again.',
-        })
+          return returnable.fail({ isSuccessful: false, shouldRetry: true })
+        } else if (code === AuthErrorCodes.NEED_CONFIRMATION) {
+          toast({
+            variant: 'destructive',
+            title: 'Please try signing in with Google!',
+            description: 'You signed up with Google, please log in with it again.',
+          })
 
-        return returnable.fail({ isSuccessful: false, shouldRetry: true })
-      } else if (code === AuthErrorCodes.EMAIL_CHANGE_NEEDS_VERIFICATION) {
-        toast({
-          variant: 'destructive',
-          title: 'Please verify your email!',
-          description: 'In order to sign in, you need to verify your new email.',
-        })
+          return returnable.fail({ isSuccessful: false, shouldRetry: true })
+        } else if (code === AuthErrorCodes.EMAIL_CHANGE_NEEDS_VERIFICATION) {
+          toast({
+            variant: 'destructive',
+            title: 'Please verify your email!',
+            description: 'In order to sign in, you need to verify your new email.',
+          })
 
-        return returnable.fail({ isSuccessful: false, shouldRetry: true })
+          return returnable.fail({ isSuccessful: false, shouldRetry: true })
+        }
+      }
+    } else if (mode === AUTH_MODE.SIGN_UP) {
+      if ((error as FirebaseError).code) {
+        const code = (error as FirebaseError).code as typeof AuthErrorCodes[keyof typeof AuthErrorCodes]
+        if (code === AuthErrorCodes.EMAIL_EXISTS) {
+          // NOTE: How will this ever get triggered?
+          toast({
+            variant: 'destructive',
+            title: 'Email address already in use!',
+            description: 'Please try creating an account with a different email address.'
+          })
+
+          return returnable.fail({ isSuccessful: false, shouldRetry: false })
+        } else if (
+          code === AuthErrorCodes.INVALID_EMAIL ||
+          code === AuthErrorCodes.INVALID_PASSWORD
+        ) {
+          toast({
+            variant: 'destructive',
+            title: 'Invalid email address or password!',
+            description: 'Please enter the correct email and password.',
+          })
+
+          return returnable.fail({ isSuccessful: false, shouldRetry: true })
+        } else if (code === AuthErrorCodes.NEED_CONFIRMATION) {
+          toast({
+            variant: 'destructive',
+            title: 'Please try signing in with Google!',
+            description: 'You signed up with Google, please log in with it again.',
+          })
+
+          return returnable.fail({ isSuccessful: false, shouldRetry: false })
+        } else if (code === AuthErrorCodes.WEAK_PASSWORD) {
+          toast({
+            variant: 'destructive',
+            title: 'Please use a stronger password!',
+            description: 'Your current password is weak, please use a stronger one.',
+          })
+
+          return returnable.fail({ isSuccessful: false, shouldRetry: true })
+        }
       }
     }
 
@@ -119,6 +162,16 @@ export const authenticateWithEmailAndPassword = async (
       variant: 'destructive',
       title: 'Uh oh! Something went wrong.',
       description: "We're currently facing some problems, please try again later!",
+    })
+
+    logError({
+      functionName: 'authenticateWithEmailAndPassword',
+      data: {
+        emailAddress,
+        password,
+        mode,
+      },
+      error,
     })
 
     return returnable.fail({ isSuccessful: false, shouldRetry: true })
@@ -137,7 +190,9 @@ export const authenticateWithGoogle = async (
 
     // NOTE: Check if the user was just created via Google Auth
     const userSnapshot = await getRDBUserSnapshot(UID)
-    if (userSnapshot.exists()) {
+    if (!userSnapshot.status) throw userSnapshot.payload
+
+    if (userSnapshot.payload.exists()) {
       toast({
         title: 'Logged in successfully!',
         description: 'Welcome to OpenReply.',
@@ -154,7 +209,11 @@ export const authenticateWithGoogle = async (
 
     return returnable.success(credential)
   } catch (error) {
-    console.error(error)
+    logError({
+      functionName: 'authenticateWithGoogle',
+      data: null,
+      error,
+    })
     toast({
       variant: 'destructive',
       title: 'Uh oh! Something went wrong.',
--- entrypoints/content/firebase/realtime-database/user/index.ts
+++ entrypoints/content/firebase/realtime-database/user/index.ts
@@ -1,6 +1,24 @@
 // Packages:
 import { database } from '../..'
 import { child, get, ref } from 'firebase/database'
+import returnable from '@/entrypoints/content/utils/returnable'
+import logError from '@/entrypoints/content/utils/logError'
+
+// Typescript:
+import type { Returnable } from '@/entrypoints/content/types'
+import type { DataSnapshot } from 'firebase/database'
 
 // Exports:
-export const getRDBUserSnapshot = async (UID: string) => await get(child(ref(database), `users/${ UID }`))
+export const getRDBUserSnapshot = async (UID: string): Promise<Returnable<DataSnapshot, Error>> => {
+  try {
+    return returnable.success(await get(child(ref(database), `users/${ UID }`)))
+  } catch (error) {
+    logError({
+      functionName: 'getRDBUserSnapshot',
+      data: UID,
+      error: error,
+    })
+
+    return returnable.fail(error as unknown as Error)
+  }
+}
