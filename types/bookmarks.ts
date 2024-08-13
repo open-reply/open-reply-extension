52d1b6f4bf3692420aa8f84ae1bfc8171214f550
Dirag Biswas <hey@diragb.dev>
2024-08-13 12:04:46 +0530

    feat: add `RealtimeDatabaseBookmarks` to RDB schema

--- types/bookmarks.ts
+++ types/bookmarks.ts
@@ -2,6 +2,7 @@
 import type { FieldValue } from 'firebase/firestore'
 import type { CommentID } from './comments-and-replies'
 import type { URLHash } from './websites'
+import type { UID } from './user'
 
 // Exports:
 /**
@@ -37,4 +38,19 @@ export interface ReplyBookmark extends CommentBookmark {
    * The ID of the comment the reply is under.
    */
   commentID: CommentID
+}
+
+/**
+ * The `RealtimeBookmarkStats` defines the base statistics associated with bookmarked content.
+ */
+export interface RealtimeBookmarkStats {
+  /**
+   * The map of all the users who have bookmarked this content. This is useful for quick look-ups and should not be queried.
+   */
+  bookmarkedBy: Record<UID, boolean>
+
+  /**
+   * The total number of users that have bookmarked this content.
+   */
+  bookmarkCount: number
 }
