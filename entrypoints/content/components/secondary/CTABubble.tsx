9b35c838ae24150c989795394b4da3e50fa2d4f0
Dirag Biswas <hey@diragb.dev>
2024-08-23 00:38:32 +0530

    feat: fade-in CTA bubble

--- entrypoints/content/components/secondary/CTABubble.tsx
+++ entrypoints/content/components/secondary/CTABubble.tsx
@@ -1,11 +1,24 @@
 // Packages:
+import { useState, useEffect } from 'react'
 import useUtility from '../../hooks/useUtility'
 import { cn } from '../../lib/utils'
+
+// Imports:
+import IconLightTransparent from '~/assets/icon-light-transparent.png'
+import IconDarkTransparent from '~/assets/icon-dark-transparent.png'
 
 // Functions:
 const CTABubble = () => {
   // Constants:
   const { isActive, setIsActive } = useUtility()
+
+  // State:
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  // Effects:
+  useEffect(() => {
+    setIsLoaded(true)
+  }, [])
 
   // Return:
   return (
@@ -13,9 +26,15 @@ const CTABubble = () => {
       className={
         cn(
           'absolute z-[1] bottom-12 -left-24 w-12 h-12 bg-white rounded-full shadow-sm cursor-pointer hover:brightness-110 hover:shadow-md transition-all duration-300',
-          isActive ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'
+          (isActive || !isLoaded) ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto',
         )
       }
+      style={{
+        backgroundImage: `url(${IconLightTransparent})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
       onClick={() => setIsActive(true)}
     />
   )
