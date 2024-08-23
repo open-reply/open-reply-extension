60a9ee89319c91ca28ada39b99daa0a9d0c382df
Dirag Biswas <hey@diragb.dev>
2024-08-24 04:41:20 +0530

    feat: add `useAuth` to `/`

--- entrypoints/content/pages/index.tsx
+++ entrypoints/content/pages/index.tsx
@@ -1,5 +1,6 @@
 // Packages:
 import { useNavigate } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 // Constants:
 import ROUTES from '../routes'
@@ -7,15 +8,26 @@ import ROUTES from '../routes'
 const Index = () => {
   // Constants:
   const navigate = useNavigate()
+  const {
+    isLoading,
+    isSignedIn,
+    isAccountFullySetup,
+  } = useAuth()
 
   // Effects:
   useEffect(() => {
-    const isAuthenticated = false
-
-    if (!isAuthenticated) {
-      navigate(ROUTES.AUTHENTICATION)
+    if (!isLoading) {
+      if (isSignedIn) {
+        if (isAccountFullySetup) navigate(ROUTES.WEBSITE)
+        else navigate(ROUTES.SETUP_ACCOUNT)
+      } else navigate(ROUTES.AUTHENTICATION)
     }
-  }, [navigate])
+  }, [
+    isLoading,
+    isSignedIn,
+    isAccountFullySetup,
+    navigate,
+  ])
 
   // Return:
   return <></>
