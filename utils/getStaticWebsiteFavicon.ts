ffb590b4f469c9d0c7eed5b83fe194aad19dd1c0
Dirag Biswas <hey@diragb.dev>
2024-09-13 23:34:18 +0530

    feat: add `getStaticWebsiteFavicon`

---
+++ utils/getStaticWebsiteFavicon.ts
@@ -0,0 +1,8 @@
+// Functions:
+/**
+ * Provides the URL for the static favicon served by Google.
+ */
+const getStaticWebsiteFavicon = (domain: string) => `https://www.google.com/s2/favicons?sz=64&domain_url=${ domain }`
+
+// Exports:
+export default getStaticWebsiteFavicon
