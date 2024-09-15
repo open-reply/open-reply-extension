6d8439181697d93171fbd622c35266fb2ed8a8de
Dirag Biswas <hey@diragb.dev>
2024-09-15 23:00:03 +0530

    fix: types and linting

--- entrypoints/content/components/tertiary/FormProfile.tsx
+++ entrypoints/content/components/tertiary/FormProfile.tsx
@@ -1,17 +1,17 @@
 //Packages:
-import { useState, useEffect } from "react";
-import useUserPreferences from "../../hooks/useUserPreferences";
+import { useState, useEffect } from 'react'
+import useUserPreferences from '../../hooks/useUserPreferences'
 
 // Components:
-import { Separator } from "../ui/separator";
-import { Switch } from "../ui/switch";
-import { Slider } from "../ui/slider";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Label } from "../ui/label";
+import { Separator } from '../ui/separator'
+import { Switch } from '../ui/switch'
+import { Slider } from '../ui/slider'
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
+import { Label } from '../ui/label'
 
 const FormProfile = () => {
   //State:
-  const [radioValue, setradioValue] = useState("option-one");
+  const [radioValue, setradioValue] = useState('option-one')
 
   //Constants:
   const {
@@ -21,96 +21,96 @@ const FormProfile = () => {
     setWebsiteWarningWarnAt,
     setWebsiteWarningPosition,
     loadUserPreferences,
-  } = useUserPreferences();
+  } = useUserPreferences()
 
   //Effects:
   useEffect(() => {
     async function handleLoad() {
-      console.log(await loadUserPreferences());
+      console.log(await loadUserPreferences())
     }
-    handleLoad();
-  }, []);
+    handleLoad()
+  }, [])
 
   //Return:
   return (
     <>
       {isLoading ? (
         <>
-          <span className="flex flex-col">
-            <h1 className="text-2xl font-medium">Profile</h1>
-            <h2 className="text-base font-regular text-brand-secondary">
+          <span className='flex flex-col'>
+            <h1 className='text-2xl font-medium'>Profile</h1>
+            <h2 className='text-base font-regular text-brand-secondary'>
               Manage your safety on the internet.
             </h2>
           </span>
-          <span className="flex flex-col">
-            <h3 className="text-base font-semibold">Website Warning</h3>
-            <p className="text-xs font-regular text-brand-secondary">
-              Control how safety banners are shown on unsafe websites.{" "}
+          <span className='flex flex-col'>
+            <h3 className='text-base font-semibold'>Website Warning</h3>
+            <p className='text-xs font-regular text-brand-secondary'>
+              Control how safety banners are shown on unsafe websites.{' '}
             </p>
           </span>
-          <span className="flex flex-row justify-between h-fit align-middle items-center">
-            <h3 className="text-sm font-regular">Display Safety Banner</h3>
-            <Switch className="scale-75" />
+          <span className='flex flex-row justify-between h-fit align-middle items-center'>
+            <h3 className='text-sm font-regular'>Display Safety Banner</h3>
+            <Switch className='scale-75' />
           </span>
-          <span className="flex flex-col gap-1">
-            <h3 className="text-sm font-regular">Intelligent Warning</h3>
-            <p className="text-xs font-regular text-brand-secondary pb-1">
+          <span className='flex flex-col gap-1'>
+            <h3 className='text-sm font-regular'>Intelligent Warning</h3>
+            <p className='text-xs font-regular text-brand-secondary pb-1'>
               Not all websites are equally dangerous. Choose when we should warn
-              you, based on the website’s risk level.{" "}
+              you, based on the website’s risk level.{' '}
             </p>
-            <Slider step={25} thumbprop="h-4 w-4" />
-            <div className="flex flex-row justify-between pt-1">
-              <span className="text-xs font-regular text-brand-secondary">
+            <Slider step={25} thumbClassName='h-4 w-4' />
+            <div className='flex flex-row justify-between pt-1'>
+              <span className='text-xs font-regular text-brand-secondary'>
                 Minimal
               </span>
-              <span className="text-xs font-regular text-brand-secondary">
+              <span className='text-xs font-regular text-brand-secondary'>
                 Moderate
               </span>
-              <span className="text-xs font-regular text-brand-secondary">
+              <span className='text-xs font-regular text-brand-secondary'>
                 Severe
               </span>
             </div>
           </span>
-          <span className="flex flex-col gap-1">
-            <h3 className="text-sm font-regular">Safety Banner Position</h3>
-            <p className="text-xs font-regular text-brand-secondary pb-3">
-              Select where the safety banner should be shown.{" "}
+          <span className='flex flex-col gap-1'>
+            <h3 className='text-sm font-regular'>Safety Banner Position</h3>
+            <p className='text-xs font-regular text-brand-secondary pb-3'>
+              Select where the safety banner should be shown.{' '}
             </p>
-            <div className="flex flex-row justify-start gap-10 w-2/3 min-h-24">
+            <div className='flex flex-row justify-start gap-10 w-2/3 min-h-24'>
               <RadioGroup
-                defaultValue="option-one"
+                defaultValue='option-one'
                 onValueChange={(e) => setradioValue(e)}
-                className="flex flex-col gap-2"
+                className='flex flex-col gap-2'
               >
-                <div className="flex items-center gap-2">
+                <div className='flex items-center gap-2'>
                   <RadioGroupItem
-                    value="option-one"
-                    id="option-one"
-                    className="h-3 w-3"
-                    fillprop="h-1 w-1"
+                    value='option-one'
+                    id='option-one'
+                    className='h-3 w-3'
+                    indicatorCircleClassName='h-1 w-1'
                   />
-                  <Label htmlFor="option-one" className="text-xs">
+                  <Label htmlFor='option-one' className='text-xs'>
                     Top
                   </Label>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className='flex items-center gap-2'>
                   <RadioGroupItem
-                    value="option-two"
-                    id="option-two"
-                    className="h-3 w-3"
-                    fillprop="h-1 w-1"
+                    value='option-two'
+                    id='option-two'
+                    className='h-3 w-3'
+                    indicatorCircleClassName='h-1 w-1'
                   />
-                  <Label htmlFor="option-two" className="text-xs">
+                  <Label htmlFor='option-two' className='text-xs'>
                     Bottom
                   </Label>
                 </div>
               </RadioGroup>
-              <div className={"w-1/2 bg-overlay rounded-sm relative"}>
+              <div className={'w-1/2 bg-overlay rounded-sm relative'}>
                 <div
                   className={`absolute w-full bg-rose-500 h-[10%] transition-[bottom] duration-700 ${
-                    radioValue === "option-one"
-                      ? "rounded-t-sm bottom-[calc(100%-10%)]"
-                      : "rounded-b-sm bottom-0"
+                    radioValue === 'option-one'
+                      ? 'rounded-t-sm bottom-[calc(100%-10%)]'
+                      : 'rounded-b-sm bottom-0'
                   }`}
                 ></div>
               </div>
@@ -118,10 +118,10 @@ const FormProfile = () => {
           </span>
         </>
       ) : (
-        "POTTY"
+        'POTTY'
       )}
     </>
-  );
-};
+  )
+}
 
-export default FormProfile;
+export default FormProfile
--- entrypoints/content/components/ui/radio-group.tsx
+++ entrypoints/content/components/ui/radio-group.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
-import { Circle } from "lucide-react";
+import * as React from 'react'
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { Circle } from 'lucide-react'
 
-import { cn } from "@/entrypoints/content/lib/utils";
+import { cn } from '@/entrypoints/content/lib/utils'
 
 const RadioGroup = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Root>,
@@ -10,35 +10,35 @@ const RadioGroup = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Root
-      className={cn("grid gap-2", className)}
+      className={cn('grid gap-2', className)}
       {...props}
       ref={ref}
     />
-  );
-});
-RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+  )
+})
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ fillprop, className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> & { indicatorCircleClassName?: string }
+>(({ indicatorCircleClassName, className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+      <RadioGroupPrimitive.Indicator className='flex items-center justify-center'>
         <Circle
-          className={cn("h-2.5 w-2.5 fill-current text-current", fillprop)}
+          className={cn('h-2.5 w-2.5 fill-current text-current', indicatorCircleClassName)}
         />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
-  );
-});
-RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+  )
+})
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
 
-export { RadioGroup, RadioGroupItem };
+export { RadioGroup, RadioGroupItem }
