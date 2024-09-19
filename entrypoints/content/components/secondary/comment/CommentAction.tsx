c5e6fb7781ad2b96c684cb53776d46dc78bdfc5c
Boobalan Shettiyar <boobalan.shettiyar@Boobalans-MacBook-Pro.local>
2024-09-20 00:54:41 +0530

    fix upvote comment logic

--- entrypoints/content/components/secondary/comment/CommentAction.tsx
+++ entrypoints/content/components/secondary/comment/CommentAction.tsx
@@ -1,73 +1,132 @@
-
 // Packages:
-import { useState } from 'react'
+import { useState } from "react";
 
 // Typescript:
-import { VoteCount } from 'types/votes'
+import { VoteCount } from "types/votes";
 interface VoteCountProps {
-  voteCount: VoteCount
+  voteCount: VoteCount;
 }
 
 enum VoteStatus {
-  UPVOTED = 'UPVOTED',
-  DOWNVOTED = 'DOWNVOTED'
+  UPVOTED = "UPVOTED",
+  DOWNVOTED = "DOWNVOTED",
 }
 
 // Imports::
-import { ArrowBigDown, ArrowBigUp, Ellipsis, Forward, MessageSquare } from 'lucide-react'
+import {
+  ArrowBigDown,
+  ArrowBigUp,
+  Ellipsis,
+  Forward,
+  MessageSquare,
+} from "lucide-react";
 
-const CommentAction: React.FC<VoteCountProps> = ({ voteCount: { up = 0, down = 0 } }) => {
-
-  const [voteCount, setVoteCount] = useState(up - down)
-  const [voteStatus, setVoteStatus] = useState<VoteStatus>()
+const CommentAction: React.FC<VoteCountProps> = ({ voteCount: { up, down } }) => {
+  const [voteCount, setVoteCount] = useState(0);
+  const [voteStatus, setVoteStatus] = useState<VoteStatus | null>();
 
   useEffect(() => {
-    setVoteCount(up - down)
-  }, [up, down])
+    setVoteCount(up - down);
+  }, [up, down]);
 
-  // #TODO: Logic is incorrect need to fix this
+  useEffect(() => {
+    console.log(voteCount, voteStatus)
+  }, [voteCount, voteStatus])
+
   // Maybe take all the states into one single object state
   const upvoteComment = () => {
-    if (voteStatus === VoteStatus.DOWNVOTED) {
-      setVoteStatus(VoteStatus.UPVOTED)
+    
+    /**
+     * if its already upvoted then turn off upvote and decrease vote count by 1
+     */
+    if (voteStatus === VoteStatus.UPVOTED) {
+      setVoteStatus(null)
       setVoteCount(voteCount - 1)
       return
     }
 
-    setVoteStatus(VoteStatus.UPVOTED)
-    setVoteCount(voteCount + 1)
-  }
-  const downvoteComment =  () => {
-    if (voteStatus === VoteStatus.UPVOTED) {
-      setVoteStatus(VoteStatus.DOWNVOTED)
+    /**
+     * if its downvoted then set votestatus to be upvoted and increase the votecount to be +2
+     * to offset the votecount that was already decreased by 1 when it was downvoted
+     */
+    if (voteStatus === VoteStatus.DOWNVOTED) {
+      setVoteStatus(VoteStatus.UPVOTED)
+      setVoteCount(voteCount + 2)
+      return
+    }
+
+    /**
+     * if null: then simply increase the vote count and set votestatus to be upvoted
+     */
+    setVoteStatus(VoteStatus.UPVOTED);
+    setVoteCount(voteCount + 1);
+  };
+
+  const downvoteComment = () => {
+    // if its already downvoted then turn off downvote and increase votecount by 1
+    if (voteStatus === VoteStatus.DOWNVOTED) {
+      setVoteStatus(null)
       setVoteCount(voteCount + 1)
       return
     }
-    setVoteStatus(VoteStatus.DOWNVOTED)
-    setVoteCount(voteCount - 1)
-  }
+
+    /** 
+     * if its already upvoted then set it to be downvoted and decrease votecount by 2 to offset 
+     * the votecount that was already increased by 1 when it was upvoted
+     * */ 
+    if (voteStatus === VoteStatus.UPVOTED) {
+      setVoteStatus(VoteStatus.DOWNVOTED)
+      setVoteCount(voteCount - 2)
+      return
+    }
+
+    /** 
+     * if nothing simply set votestatus to be downvoted and decrease votecount by 1
+     * */ 
+    setVoteStatus(VoteStatus.DOWNVOTED);
+    setVoteCount(voteCount - 1);
+  };
+
+  const isUpvoted = useMemo(
+    () => voteStatus === VoteStatus.UPVOTED,
+    [voteStatus]
+  );
+  const isDownvoted = useMemo(
+    () => voteStatus === VoteStatus.DOWNVOTED,
+    [voteStatus]
+  );
 
   return (
-    <div className='flex space-x-6 items-center pt-1 -ml-[px]'>
-      <div className='flex space-x-2 items-center'>
-        <ArrowBigUp  size={18} fill='green'  color={'green'} onClick={upvoteComment} />
-        <p className='text-[12px]'> { voteCount } </p>
-        <ArrowBigDown size={18}  onClick={downvoteComment} />
+    <div className="flex space-x-6 items-center pt-1 -ml-[px]">
+      <div className="flex space-x-2 items-center">
+        <ArrowBigUp
+          size={18}
+          fill={isUpvoted ? "green" : "currentColor"}
+          color={isUpvoted ? "green" : "currentColor"}
+          onClick={upvoteComment}
+        />
+        <p className="text-[12px]"> {voteCount} </p>
+        <ArrowBigDown
+          size={18}
+          fill={isDownvoted ? "red" : "currentColor"}
+          color={isDownvoted ? "red" : "currentColor"}
+          onClick={downvoteComment}
+        />
       </div>
-      <div className='flex space-x-2 items-center'>
-        <MessageSquare size={14}/>
-        <p className='text-[13px]'>Reply</p>
+      <div className="flex space-x-2 items-center">
+        <MessageSquare size={14} />
+        <p className="text-[13px]">Reply</p>
       </div>
-      <div className='flex space-x-1 items-center'>
+      <div className="flex space-x-1 items-center">
         <Forward size={18} />
-        <p className='text-[13px]'>Share</p>
+        <p className="text-[13px]">Share</p>
       </div>
       <div>
         <Ellipsis size={18} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 // Exports:
-export default CommentAction
+export default CommentAction;
