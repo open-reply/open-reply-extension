fa3f98c420209104032c3d4dde4e2718ac76000c
Dirag Biswas <hey@diragb.dev>
2025-02-19 12:05:06 +0530

    feat: show banner instead of feed

--- entrypoints/content/pages/feed.tsx
+++ entrypoints/content/pages/feed.tsx
@@ -1,54 +1,63 @@
 // Packages:
-import useUserPreferences from '../hooks/useUserPreferences'
+// import useUserPreferences from '../hooks/useUserPreferences'
 
-// Typescript:
-import { UnsafeContentPolicy } from 'types/user-preferences'
-import { Comment as CommentInterface } from 'types/comments-and-replies'
+// // Typescript:
+// import { UnsafeContentPolicy } from 'types/user-preferences'
+// import { Comment as CommentInterface } from 'types/comments-and-replies'
 
-// Constants:
-import { commentFixtures } from '@/fixtures/comment'
+// // Constants:
+// import { commentFixtures } from '@/fixtures/comment'
 
-// Components:
-import { ScrollArea } from '../components/ui/scroll-area'
-import Comment from '../components/tertiary/Comment'
+// // Components:
+// import { ScrollArea } from '../components/ui/scroll-area'
+// import Comment from '../components/tertiary/Comment'
 
 // Functions:
 const Feed = () => {
-  // Constants:
-  const { moderation } = useUserPreferences()
+  // // Constants:
+  // const { moderation } = useUserPreferences()
 
   // Return:
+  // return (
+  //   <main className='w-full pt-[68px] bg-white' style={{ height: 'calc(100% - 68px)' }}>
+  //     <ScrollArea className='w-full h-screen' hideScrollbar>
+  //       <div className='flex flex-col gap-4 w-full px-4 pt-7 pb-16'>
+  //         {[...commentFixtures, ...commentFixtures]
+  //           .filter(
+  //             comment =>
+  //               !comment.isDeleted &&
+  //               !comment.isRemoved &&
+  //               !comment.isRestricted &&
+  //               (moderation.unsafeContentPolicy === UnsafeContentPolicy.FilterUnsafeContent
+  //                 ? !comment.hateSpeech.isHateSpeech
+  //                 : true)
+  //           )
+  //           .map(comment => (
+  //             <Comment
+  //               comment={comment}
+  //               key={comment.id}
+  //               updateCommentLocally={(comment: CommentInterface) => {
+  //                 // setComments(_comments => _comments.map(_comment => {
+  //                 //   if (_comment.id === comment.id) return {
+  //                 //     ..._comment,
+  //                 //     ...comment,
+  //                 //   }
+  //                 //   else return _comment
+  //                 // }))
+  //               }}
+  //             />
+  //           ))}
+  //       </div>
+  //     </ScrollArea>
+  //   </main>
+  // )
+
   return (
-    <main className='w-full pt-[68px] bg-white' style={{ height: 'calc(100% - 68px)' }}>
-      <ScrollArea className='w-full h-screen' hideScrollbar>
-        <div className='flex flex-col gap-4 w-full px-4 pt-7 pb-16'>
-          {[...commentFixtures, ...commentFixtures]
-            .filter(
-              comment =>
-                !comment.isDeleted &&
-                !comment.isRemoved &&
-                !comment.isRestricted &&
-                (moderation.unsafeContentPolicy === UnsafeContentPolicy.FilterUnsafeContent
-                  ? !comment.hateSpeech.isHateSpeech
-                  : true)
-            )
-            .map(comment => (
-              <Comment
-                comment={comment}
-                key={comment.id}
-                updateCommentLocally={(comment: CommentInterface) => {
-                  // setComments(_comments => _comments.map(_comment => {
-                  //   if (_comment.id === comment.id) return {
-                  //     ..._comment,
-                  //     ...comment,
-                  //   }
-                  //   else return _comment
-                  // }))
-                }}
-              />
-            ))}
-        </div>
-      </ScrollArea>
+    <main className='flex justify-center items-center flex-col gap-2 w-full pt-[68px] bg-white' style={{ height: 'calc(100% - 68px)' }}>
+      <span className='text-4xl font-semibold text-center'>OpenReply</span>
+      <div className='flex items-center gap-1.5 text-sm font-medium text-brand-secondary text-center'>
+        <span>The internet’s comment section. Made in India.</span>
+      </div>
     </main>
   )
 }
